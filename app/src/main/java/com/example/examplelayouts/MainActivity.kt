06353afd8ac386d736123360395459280323b1f9
10-kapilkori <kapilkori1021@gmail.com>
2022-07-03 19:53:57 +0530

    added custom dropdowns

--- app/src/main/java/com/example/examplelayouts/MainActivity.kt
+++ app/src/main/java/com/example/examplelayouts/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.examplelayouts
 
+import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -14,7 +15,9 @@ import com.example.examplelayouts.databinding.CustomAdapterBinding
 
 class MainActivity : AppCompatActivity(), CustomAdapter.OnItemClick {
     lateinit var binding: ActivityMainBinding
-    var flag = false
+    private var flag = false
+    private val continentsList =
+        listOf<String>("Asia", "Africa", "Antarctica", "South America", "North America", "Europe")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,36 +26,50 @@ class MainActivity : AppCompatActivity(), CustomAdapter.OnItemClick {
 
         binding.apply {
             tv.setOnClickListener {
-                if (rv.visibility == View.GONE) {
-                    flag = true
-                    rv.visibility = View.VISIBLE
-                } else {
-                    flag = false
-                    rv.visibility = View.GONE
-                }
+                setAdapter()
+            }
 
-                val adapter = CustomAdapter(this@MainActivity)
-                rv.layoutManager = LinearLayoutManager(this@MainActivity)
-                rv.adapter = adapter
-
-                menu.setOnFocusChangeListener { view, b ->
-                    if (b) {
-
-                    } else {
-
-                    }
-                }
+            menu.setEndIconOnClickListener {
+                setAdapter()
             }
         }
     }
 
-    override fun onClickItem(position: Int) {
-        binding.tv.setText(resources.getString(R.string.app_name) + " " + position)
-        binding.menu.isHintEnabled = true
+    private fun setAdapter() {
+        binding.apply {
+            if (rv.visibility == View.GONE) {
+                flag = true
+                rv.visibility = View.VISIBLE
+                menu.endIconDrawable =
+                    resources.getDrawable(R.drawable.ic_baseline_arrow_drop_down_24)
+                menu.setEndIconTintList(ColorStateList.valueOf(resources.getColor(R.color.purple_700)))
+            } else {
+                flag = false
+                rv.visibility = View.GONE
+                menu.endIconDrawable =
+                    resources.getDrawable(R.drawable.ic_baseline_arrow_drop_up_24)
+            }
+
+            val adapter = CustomAdapter(this@MainActivity, continentsList)
+            rv.layoutManager = LinearLayoutManager(this@MainActivity)
+            rv.adapter = adapter
+        }
+    }
+
+    override fun onClickItem(position: Int, list: List<String>) {
+        Toast.makeText(this, position.toString(), Toast.LENGTH_SHORT).show()
+        binding.tv.setText(list[position] + " is at position: " + position)
+        binding.menu.hintTextColor = ColorStateList.valueOf(resources.getColor(R.color.purple_700))
+        binding.menu.hint = "Continents"
+        binding.rv.visibility = View.GONE
     }
 }
 
-class CustomAdapter(private val listener: OnItemClick) : RecyclerView.Adapter<CustomViewHolder>() {
+class CustomAdapter(
+    private val listener: OnItemClick,
+    private val list: List<String>
+) :
+    RecyclerView.Adapter<CustomViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
         val binding = CustomAdapterBinding.bind(
             LayoutInflater.from(parent.context).inflate(R.layout.custom_adapter, parent, false)
@@ -63,14 +80,15 @@ class CustomAdapter(private val listener: OnItemClick) : RecyclerView.Adapter<Cu
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         holder.binding.root.setOnClickListener {
-            listener.onClickItem(position)
+            listener.onClickItem(position, list)
         }
+        holder.onBind(position, list)
     }
 
-    override fun getItemCount(): Int = 10
+    override fun getItemCount(): Int = list.size
 
     interface OnItemClick {
-        fun onClickItem(position: Int)
+        fun onClickItem(position: Int, list: List<String>)
     }
 }
 
@@ -78,4 +96,7 @@ class CustomViewHolder(itemView: CustomAdapterBinding) :
     RecyclerView.ViewHolder(itemView.root) {
 
     val binding = itemView
+    fun onBind(position: Int, list: List<String>) {
+        binding.textView.text = list[position]
+    }
 }
